22bbb8a4de6f2622e47668b2915fa8a830c2bd7f
Kamal Hamza <khamsa.kamal48@gmail.com>
2023-11-29 18:47:46 +0530

    Updated code to fix URL encoding issues during POST request

--- Upload Donation.py
+++ Upload Donation.py
@@ -665,17 +665,16 @@ def post_request_re(url, params):
         'Cache-Control': 'no-cache'
     }
 
-    # # Convert int64 to int in params
-    # params = {k: int(v) if isinstance(v, np.int64) else v for k, v in params.items()}
+    # Convert int64 to int in params
+    params = {k: int(v) if isinstance(v, np.int64) else v for k, v in params.items()}
 
     try:
         if '&' in str(params):
-            # Convert int64 to int in params
-            params = {k: quote_plus(str(v)) if isinstance(v, str) else int(v) if isinstance(v, np.int64) else v for k, v in params.items()}
-            re_api_response = http.post(url, headers=headers, json=params)
+            # Quote_plus for encoding special characters in URL
+            encoded_params = {k: quote_plus(str(v)) for k, v in params.items()}
+            re_api_response = http.post(url, headers=headers, json=encoded_params)
         else:
             # Convert int64 to int in params
-            params = {k: int(v) if isinstance(v, np.int64) else v for k, v in params.items()}
             re_api_response = http.post(url, params=params, headers=headers, json=params)
 
         re_api_response.raise_for_status()  # Raises a HTTPError if the status is 4xx, 5xx
