88a37c8a4eeee8f5046127864cbc1044d1890a1c
Kamal Hamza <khamsa.kamal48@gmail.com>
2023-11-30 18:31:23 +0530

    Added fix to consider only TRUE constituents

--- Upload Donation.py
+++ Upload Donation.py
@@ -805,6 +805,7 @@ def search_constituent(email, phone, pan):
             response = get_request_re(url, params)
 
             if response['count'] != 0:
+                # Lookup ID is only present in TRUE constituents
                 return set(int(x['id']) for x in response['value'][:] if x['lookup_id'])
 
         i += 1
